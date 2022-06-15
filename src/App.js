43031b0a82b241086e5f73fd5860573214afea17
Shoxidbek Kimsanboyev <shox7316@gmail.com>
2022-06-15 08:12:18 +0500

    crm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Login from './components/login/Login';
+import AdminIndex from './components/Admin/AdminIndex';
+import RecetionIndex from './components/Reception/RecetionIndex';
+import TeachersIndex from './components/Teachers/TeachersIndex';
+import {Routes, Route} from 'react-router-dom'
+import './Style/LoginStyle/LoginStyle.css'
+import StudentIndex from "./components/Students/StudentIndex";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Login/>}/>
+                <Route path='/superuser' element={<AdminIndex/>}/>
+                <Route path='/teacher' element={<TeachersIndex/>}/>
+                <Route path='/reception' element={<RecetionIndex/>}/>
+                <Route path='/studnet' element={<StudentIndex/>}/>
+
+            </Routes>
+        </div>
+
+    )
 }
 
 export default App;
